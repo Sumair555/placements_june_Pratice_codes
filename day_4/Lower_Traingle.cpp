6b9ac4edc6913bbdb3f3e25a61c253f3b5645ddc
sksumair <mohammedsaifuddinsumair_shaik@srmap.edu.in>
2025-06-05 12:15:28 +0530

    sum of non lower triangle

---
+++ day_4/Lower_Traingle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  vector<vector<int>> arr = {
+      {1, 2, 3},
+      {1, 2, 3},
+      {1, 2, 3}};
+
+  int rows = arr.size();
+  int cols = arr[0].size();
+  int sum = 0;
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      if (i < j)
+      {
+        sum += arr[i][j];
+      }
+    }
+  }
+
+  cout << sum;
+}
